6b466281bce72b628fade90c9ad578f6b84a19b9
oltjano <oltjano@baby.(none)>
2014-06-14 01:15:40 +0200

    changes

--- hackerscoops/urls.py
+++ hackerscoops/urls.py
@@ -7,6 +7,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'hackerscoops.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^home/', include('article.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
