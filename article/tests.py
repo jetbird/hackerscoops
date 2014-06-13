175f69b1e1a7b160fb4281209501eafaf3b75f9d
Hansel Dunlop <hansel@interpretthis.org>
2014-06-13 09:05:12 +0100

    Adds unit tests for the Category model, makes manage.py executable

--- article/tests.py
+++ article/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from article import models
 
-# Create your tests here.
+class CategoryTest(TestCase):
+
+    def test_model_can_be_created(self):
+
+        category = models.Category.objects.create(
+            name='Cat 1', description='Category 1 is long')
+
+        self.assertEqual(category.name, 'Cat 1')
+        self.assertEqual(category.description, 'Category 1 is long')
+
+    def test_unicode_representation(self):
+        category = models.Category.objects.create(
+            name='Cat 2', description='Category 2 is long')
+
+        self.assertEqual(unicode(category), 'Cat 2')
+
