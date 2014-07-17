270de4b8e2f3cfc03897e85b86092c7f1ee6758b
oltjano <oltjano@baby.(none)>
2014-07-17 23:58:57 +0200

    Added Pagination

--- article/views.py
+++ article/views.py
@@ -1,9 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from article.models import Article
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 def home(request):
-    return render(request, 'home.html',
-            {'articles':Article.objects.all()[::-1][:10]})
+    article_list = Article.objects.all()
+    paginator = Paginator(article_list,10)
+
+    page = request.GET.get('page')
+    try:
+        articles = paginator.page(page)
+    except PageNotAnInteger:
+        articles = paginator.page(1)
+    except EmptyPage:
+        articles = paginator.page(paginator.num_pages)
+
+    return render_to_response('home.html',
+            {'articles':articles})
 
 
 def article(request, slug):
